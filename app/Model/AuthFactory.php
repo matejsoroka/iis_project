80b0e2d3588c156b2fa2ecac0f3f97b8372b87c8
Matej Soroka <hi@matejsoroka.com>
2019-11-21 14:41:44 +0100

    permissions

--- app/Model/AuthFactory.php
+++ app/Model/AuthFactory.php
@@ -18,11 +18,42 @@ class AuthFactory
         $acl->addRole('leader', 'garant');
         $acl->addRole('admin', 'leader');
 
-        $acl->addResource('Homepage:default');
-//
+        /* Guest */
+        $acl->addResource('Curse:default'); // Zobrazenie kurzov
+        $acl->addResource('Curse:detail'); // Zobrazenie kurzov
+        $acl->allow('guest', 'Curse:detail');
+        $acl->allow('guest', 'Curse:default');
 
-//        $acl->deny('guest', 'backend');
-        $acl->allow('student', 'Homepage:default');
+        /* Student */
+        $acl->addResource('Curse:register'); // Registracia kurzov
+        $acl->allow('student', 'Curse:register');
+
+        $acl->addResource('Index:default'); // Zobrazenie hodnotenia
+        $acl->allow('student', 'Index:default');
+
+        $acl->addResource('Curse:files'); // Zobrazenie suborov
+        $acl->allow('student', 'Curse:files');
+
+        $acl->addResource('Index:timetable'); // Zobrazenie hodnotenia
+        $acl->allow('student', 'Index:timetable');
+
+        /* Lektor */
+        $acl->addResource('Index:timetable'); // Zobrazenie hodnotenia
+        $acl->allow('leander', 'Index:timetable');
+
+        $acl->addResource('Curse:edit'); // Uprava kurzu, pozor na restricted inputka
+        $acl->allow('leander', 'Curse:edit');
+
+        /* Leader */
+        $acl->addResource('Room:edit'); // Sprava miestnosti
+        $acl->allow('room', 'Room:edit');
+
+        /* Admin */
+        $acl->addResource('User:default'); // Zoznam uzivatelov
+        $acl->allow('room', 'User:default');
+
+        $acl->addResource('User:edit'); // Uprava uzivatela uzivatelov
+        $acl->allow('room', 'User:default');
 
         return $acl;
     }
