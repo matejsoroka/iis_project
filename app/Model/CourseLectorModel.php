9227771a40daec3dd09ca0f850b5a3bb80a23536
Matej Soroka <hi@matejsoroka.com>
2019-11-26 14:27:25 +0100

    lector permissions

--- app/Model/CourseLectorModel.php
+++ app/Model/CourseLectorModel.php
@@ -8,4 +8,9 @@ class CourseLectorModel extends BaseModel
 
     public $table = "course_lector";
 
+    public function isLector(int $userId, int $courseId) : bool
+    {
+        return $this->findItem(["lector_id" => $userId, "course_id" => $courseId]) ? true : false;
+    }
+
 }
